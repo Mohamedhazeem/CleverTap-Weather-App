dad9edf4bc6cb40f833543491c42846f74d428b0
Mohamed Hazeem <a.mohamedhazeem@gmail.com>
2025-12-07 11:26:17 +0530

    input changes

--- Assets/Scripts/WeatherToast.cs
+++ Assets/Scripts/WeatherToast.cs
@@ -1,32 +1,67 @@
 using System.Threading.Tasks;
 using CleverTap.WeatherSDK.WeatherAPI;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class WeatherToast : MonoBehaviour
 {
     public LocationProvider location;   // assign in Inspector
 
     private bool _locationReady = false;
+    Camera cam;
 
     private async void Start()
     {
+        cam = Camera.main;
         while (!location.IsReady)
             await Task.Delay(100);
 
         _locationReady = true;
     }
 
-    private async void OnMouseDown()
+    private void Update()
     {
-        if (!_locationReady)
+        if (Mouse.current.leftButton.wasPressedThisFrame || Touchscreen.current?.primaryTouch.press.wasPressedThisFrame == true)
         {
-            Debug.Log("Location not ready yet!");
-            WeatherManager.Instance.ShowMessageToast("Location Not Enabled.");
-            return;
+            HandleTap();
         }
+    }
+    private Vector2 GetPointerPosition()
+    {
 
-        float lat = location.Latitude;
-        float lon = location.Longitude;
-        await WeatherManager.Instance.ShowCurrentTemperatureToast(lat, lon);
+        if (Touchscreen.current != null && Touchscreen.current.press.isPressed)
+        {
+            return Touchscreen.current.position.ReadValue();
+        }
+        else if (Mouse.current != null && Mouse.current.leftButton.isPressed)
+        {
+            return Mouse.current.position.ReadValue();
+        }
+        return Vector2.zero; // fallback
+    }
+    void HandleTap()
+    {
+        Vector2 pos = GetPointerPosition();
+
+        Ray ray = cam.ScreenPointToRay(pos);
+
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            if (hit.transform == transform)
+            {
+                Debug.Log("working");
+
+                if (!_locationReady)
+                {
+                    WeatherManager.Instance.ShowMessageToast("Location Not Enabled.");
+                    return;
+                }
+
+                float lat = location.Latitude;
+                float lon = location.Longitude;
+
+                WeatherManager.Instance.ShowCurrentTemperatureToast(lat, lon);
+            }
+        }
     }
 }
